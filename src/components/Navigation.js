a6c65caaff1de5dce933b4eff584d0f2ddb1731a
XavierDimitriFoule <x.foule@gmail.com>
2025-11-02 12:26:55 -0400

    add lien actif du menu

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,21 +1,60 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Navigation = () =>{
     return(
-        <nav class="navbar navbar-expand-lg bg-light">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-2">
             <div class="container-fluid">
-                <h3 class="navbar-brand" href="#">John Doe</h3>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+                <NavLink to="/" className="navbar-brand text-uppercase">John doe</NavLink>
+                
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
                 </button>
 
-                <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <Link to="/">Home</Link>
-                        <Link to="/services">Services</Link>
-                        <Link to="/portofolio">Portofolio</Link>
-                        <Link to="/contact">Contact</Link>
-                        <Link to="/mentionslegales">Mentions légales</Link>
+                <div clasNclassNames="collapse navbar-collapse" id="navbarTogglerDemo02">
+                
+                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0 text-uppercase text-light">
+
+                        {/*Mise en place du lien actif isActive sur mes liens*/}
+                        <li>
+                            <NavLink to="/" 
+                            style={({isActive}) =>({
+                                fontWeight: isActive? 'bold' : 'normal',
+                                borderBottom: isActive? '2px solid #fff': 'none',
+                            })}
+                            className="text-light text-decoration-none mx-3">Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/services"
+                            style={({isActive}) =>({
+                                fontWeight: isActive? 'bold' : 'normal',
+                                borderBottom: isActive? '2px solid #fff': 'none',
+                            })}
+                            className="text-light text-decoration-none mx-3">Services</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/portofolio"
+                             style={({isActive}) =>({
+                                fontWeight: isActive? 'bold' : 'normal',
+                                borderBottom: isActive? '2px solid #fff': 'none',
+                            })}
+                            className="text-light text-decoration-none mx-3">Portofolio</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/contact"
+                             style={({isActive}) =>({
+                                fontWeight: isActive? 'bold' : 'normal',
+                                borderBottom: isActive? '2px solid #fff': 'none',
+                            })}
+                            className="text-light text-decoration-none mx-3">Contact</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/mentionslegales"
+                             style={({isActive}) =>({
+                                fontWeight: isActive? 'bold' : 'normal',
+                                borderBottom: isActive? '2px solid #fff': 'none',
+                            })}
+                            className="text-light text-decoration-none mx-3">Mentions légales</NavLink>
+                        </li>
                     </ul>
                 </div>
             </div>
